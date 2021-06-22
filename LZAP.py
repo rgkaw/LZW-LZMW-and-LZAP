06137832c132cfc2e031992e7c9db5645084de4d
rgkaw <raka_asadullah@yahoo.co.id>
2021-06-23 00:03:40 +0700

    LZAP, encode and decode

--- LZAP.py
+++ LZAP.py
@@ -1,6 +1,6 @@
 
-#funtsi untuk mengkompress
-def compress(array):
+#encode
+def encode(array):
     last=256
     i=0
     array_len=len(array)
@@ -28,6 +28,8 @@ def compress(array):
             last+=1
         prev_match=match
     return out
+
+#preparing output (int) to chunks of bytes
 def output_to_bytes(array):
     i=0
     bits=0
@@ -63,30 +65,138 @@ def output_to_bytes(array):
             out.append(bit_chunks)
             out.append(padding)
         else:
-            out.append(0)
+            out.append(0)           
     return bytes(out)
 
+#get key of item (idx) from dictionary
+def get_string(idx):
+    a=b''
+    for i in dictionary:
+        if dictionary[i]==idx:
+            a=i
+    return a
 
-#input file
-f=open(file=r'text_3.txt',mode='r+b')
-input_=f.read()
+#decode
+def decode(array):
+    out=b''
+    match=b''
+    prev_match=b''
+    last=256
+    c=0
+    try:
+        for i in array:
 
-print("making dictionary...")
-#make dictionary
-dictionary={bytes([i]):i for i in range(256)}
+            match=get_string(i)
+            out=out+match
+            x=b''
+            x=(prev_match)+match
+            if x not in dictionary:
+                for j in range(len(match)-1,0,-1):
+                    dictionary[prev_match+match[:-j]]=last
+                    last+=1
+                dictionary[x]=last
+                last+=1
+            prev_match=match
+    except:
+        out=out+(get_string(i))
+    return out
 
-print("\tinput size\t: ",len(input_),'bytes')
+#preparing input from chunks of bytes into integer
+def bytes_to_int(array):
+    out=[]
+    byte_chunk=0
+    bits=0
+    length=array[0]
+    padding=array[-1]
+    array=array[1:-1]
+    size=len(array)
+    i=0
+    while i<size-1:
+        while (bits<length) & (i<size-1):
 
-print("compressing...")
-output=compress(input_)
+            #print(array[i])
+            #sleep(1)
+            #print(type(byte_chunk))
+            byte_chunk=(byte_chunk<<8)+array[i]
+            bits=bits+8
+            i+=1
+           # print('1',byte_chunk,bits)
+        while bits>=length:
+            bits=bits-length
+            temp=byte_chunk>>(bits)
+            out.append(temp)
+            byte_chunk=byte_chunk&((2**bits)-1)
+    byte_chunk=(byte_chunk<<8)+array[i]
+    byte_chunk=byte_chunk>>padding
+    out.append(byte_chunk)
+    return out
 
-print("converting to bytes...")
-#convert output list into bytes
-output=output_to_bytes(output)
-print("saving file....")
-print("\toutput size:\t",len(output),"bytes")
-#WRITE FILE OUTPUT
-f=open(file=r'text_3_LZAP',mode='w+b')
-f.write(output)
-f.close()
-print("done!!")
+#compression proccess (inputting file, make dictionary, encoding, writing into files)
+def compress():
+    print('You choose to compress file. Place file to compress in the same directory as this program.')
+    input_file=str(input("Input file (ex :file.txt): "))
+    f=open(file=input_file,mode='r+b')
+    input_=f.read()
+    file_name=''
+    for i in input_file:
+        if i == '.':
+            break
+        file_name=file_name+i
+    print("making dictionary...")
+    #make dictionary
+    dictionary={bytes([i]):i for i in range(256)}
+    print("\tinput size\t: ",len(input_),'bytes')
+
+    print("compressing...")
+    output=encode(input_)
+
+    print("converting to bytes...")
+    #convert output list into bytes
+    output=output_to_bytes(output)
+    print("saving file....")
+    print("\toutput size:\t",len(output),"bytes")
+    #WRITE FILE OUTPUT
+    f=open(file=file_name+'.LZAP',mode='w+b')
+    f.write(output)
+    f.close()
+    print("done!!")
+
+#compression proccess (inputting file, make dictionary, decoding, writing into files)
+def decompress():
+    print('You choose to decompress file. Place file to decompress in the same directory as this program.')
+    input_file=input(str("Input file (ex :file.LZMW): "))
+    file_name=''
+    for i in input_file:
+        if i == '.':
+            break
+        file_name=file_name+i
+
+    f=open(file=input_file,mode='r+b')
+    input_=f.read()
+    print("\tfile size: ",len(input_),'bytes')
+    print("preparing files...")
+    input_=bytes_to_int(input_)
+    print("creating dictionary...")
+    dictionary={bytes([i]):i for i in range(256)}
+    print("decompressing...")
+    output=decode(input_)
+    print("done!")
+    print("\toutput size:\t",len(output),'\n')
+    f=open(file=file_name+'_LZAP.txt',mode='w+b')
+    f.write(output)
+    f.close()
+#main loop
+while True:
+    dictionary={bytes([i]):i for i in range(256)}
+    try:
+        choice=int(input("1. compress\n2. decompress\n0. exit\n\tyour choice:"))
+        if choice==1:
+            compress()
+        elif choice==2:
+            decompress()
+        elif choice==0:
+            break
+        else:
+            print("invalid input")
+    except:
+        print("invalid input (not integer)")
